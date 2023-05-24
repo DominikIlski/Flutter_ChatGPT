e3dac4287b5c4c3730ca5db5bfd01f9607693cf4
Dominik Ilski <dominikk.ilski@gmail.com>
2023-05-24 20:48:13 +0200

    login changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
+import 'package:chat_gpt/keys.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart';
+
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    GoogleProvider(clientId: GOOGLE_CLIENT_WEB),
+    AppleProvider(),
+  ]);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final providers = [EmailAuthProvider()];
-
     return MaterialApp(
       initialRoute:
           FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile',
       routes: {
         '/sign-in': (context) {
           return SignInScreen(
-            providers: providers,
             actions: [
               AuthStateChangeAction<SignedIn>((context, state) {
                 Navigator.pushReplacementNamed(context, '/profile');
@@ -33,7 +40,6 @@ class MyApp extends StatelessWidget {
         },
         '/profile': (context) {
           return ProfileScreen(
-            providers: providers,
             actions: [
               SignedOutAction((context) {
                 Navigator.pushReplacementNamed(context, '/sign-in');
